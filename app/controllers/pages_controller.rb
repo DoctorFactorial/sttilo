8796884b7852209049cf1f630fd9e614492660b7
Michael Gordon <mike1gordon@gmail.com>
2015-04-20 09:34:17 +0200

    Photo pages made dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
     if current_user
       redirect_to album_path
     end
+    @photos = Photo.where(category: 'Home Page')
   end
 
   def dashboard
@@ -11,23 +12,30 @@ class PagesController < ApplicationController
   end
 
   def blog
+    @photos = Photo.where(category: 'Blog')
   end
 
   def about
+    @photos = Photo.where(category: 'About')
   end
 
   def contact
+    @photos = Photo.where(category: 'Contact')
   end
 
   def album
+    @photos = Photo.where(category: 'Album')
   end
 
   def weddings
+    @photos = Photo.where(category: 'Weddings')
   end
 
   def travel
+    @photos = Photo.where(category: 'Travel')
   end
 
   def communion
+    @photos = Photo.where(category: 'Communion')
   end
 end
