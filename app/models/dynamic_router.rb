f516cef63d412702d2b66a6afcefa3812abd6a4e
Michael Gordon <mike1gordon@gmail.com>
2015-04-27 16:53:07 +0200

    Cleaned up URLs

--- app/models/dynamic_router.rb
+++ app/models/dynamic_router.rb
@@ -5,7 +5,7 @@ class DynamicRouter
 				puts "Routing #{sp.name}"
 				category = if sp.category.nil? then '' else '/' + sp.category.downcase end
 				route = if sp.category.nil? then 'newpage_' + sp.id.to_s else sp.category.downcase + '_' + sp.id.to_s end
-				get "#{category}/#{sp.name}", :to => "subpages#show", as: route, defaults: {id: sp.id}
+				get "#{category}/#{sp.slug}", :to => "subpages#show", as: route, defaults: {id: sp.id}
 			end
 		end
 	end
