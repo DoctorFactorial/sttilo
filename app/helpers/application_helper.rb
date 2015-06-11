c873589d31b910e280d9621dca1c40af64b96cd2
Michael Gordon <mike1gordon@gmail.com>
2015-06-11 16:12:02 +0200

    Statically fixed language change photo drop bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def locale_url_es(locale)
+		subpath = request.fullpath.sub(/(en)/, "#{locale}")
+  		return subpath
+	end
+	def locale_url_en(locale)
+		subpath = request.fullpath.sub(/(es)/, "#{locale}")
+  		return subpath
+	end
 end
